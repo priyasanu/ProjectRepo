a94d821c440a9ba1d0b3c978919453df46a67ed1
priyasanu <114900208+priyasanu@users.noreply.github.com>
2025-01-03 20:22:13 +0530

    Addition

---
+++ AddFloatAndInteger.java
@@ -0,0 +1,15 @@
+
+
+public class AddFloatAndIntegerSimple {
+    public static void main(String[] args) {
+       
+        float floatNumber = 12.5f; 
+        int intNumber = 8;         
+
+       
+        float sum = floatNumber + intNumber;
+
+        // Print the result
+        System.out.println("The sum of " + floatNumber + " and " + intNumber + " is: " + sum);
+    }
+}
